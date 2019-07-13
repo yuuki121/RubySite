7b96ed240ae763d92c7fd9434241b70309cfb141
yuuki121 <yuuki_kind@icloud.com>
2019-07-13 12:49:01 +0900

    第1章

--- ruby_app/app/controllers/application_controller.rb
+++ ruby_app/app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  def hello
+    render html: "hello, world!"
+  end
+
+  def goodbye
+    render html: "goodbye, world!"
+  end
+
 end
